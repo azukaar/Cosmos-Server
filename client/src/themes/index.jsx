89defa2c04abf7b4c27b1ef0d27a19dedcd4161e
Yann Stepienik <azukaar@gmail.com>
2023-05-02 00:53:46 +0100

    [release] v0.3.1  - fix overflowing text in home page

--- client/src/themes/index.jsx
+++ client/src/themes/index.jsx
@@ -31,7 +31,8 @@ export default function ThemeCustomization({ children }) {
                     sm: 768,
                     md: 1024,
                     lg: 1266,
-                    xl: 1536
+                    xl: 1536,
+                    xxl: 1920,
                 }
             },
             direction: 'ltr',
