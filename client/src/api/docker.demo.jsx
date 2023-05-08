b22c83333da2ebd6f2d02351e5b238d004c2b418
Yann Stepienik <azukaar@gmail.com>
2023-05-08 16:30:21 +0100

    [skip ci] demo update

--- client/src/api/docker.demo.jsx
+++ client/src/api/docker.demo.jsx
@@ -1,8 +1,18 @@
 import configDemo from './docker.demo.json';
+import configDemoCont from './container.demo.json';
+import volumesDemo from './volumes.demo.json';
+import networkDemo from './networks.demo.json';
+import logDemo from './logs.demo.json';
 
 function list() {
   return new Promise((resolve, reject) => {
     resolve(configDemo)
+  });
+}
+
+function get(containerName) {
+  return new Promise((resolve, reject) => {
+    resolve(configDemoCont)
   });
 }
 
@@ -24,15 +34,138 @@ const newDB = () => {
     
 const manageContainer = () => {
   return new Promise((resolve, reject) => {
-    resolve({
-      "status": "ok",
-    })
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
   });
 }
-    
+
+function updateContainer(containerId, values) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function listContainerNetworks(containerId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function createNetwork(values) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function attachNetwork(containerId, networkId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function detachNetwork(containerId, networkId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function createVolume(values) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function volumeList() {
+  return new Promise((resolve, reject) => {
+    resolve(volumesDemo)
+  });
+}
+
+function volumeDelete(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function networkList() {
+  return new Promise((resolve, reject) => {
+    resolve(networkDemo)
+  });
+}
+
+function networkDelete(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        "status": "ok",
+      })},
+      2000
+    );
+  });
+}
+
+function getContainerLogs(containerId, searchQuery, limit, lastReceivedLogs, errorOnly) {
+  if(limit < 50) limit = 50;
+
+  return new Promise((resolve, reject) => {
+    resolve(logDemo)
+  });
+}
+
 export {
   list,
+  get,
   newDB,
   secure,
-  manageContainer
+  manageContainer,
+  volumeList,
+  volumeDelete,
+  networkList,
+  networkDelete,
+  getContainerLogs,
+  updateContainer,
+  listContainerNetworks,
+  createNetwork,
+  attachNetwork,
+  detachNetwork,
+  createVolume,
 };
