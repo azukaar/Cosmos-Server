ad95b4acb597266c2405a43364af659394555cb9
Yann Stepienik <azukaar@gmail.com>
2023-05-28 12:37:14 +0100

    [release] v0.5.12-unstable

--- src/docker/docker.go
+++ src/docker/docker.go
@@ -89,8 +89,6 @@ func Connect() error {
 }
 
 func RecreateContainer(containerID string, containerConfig types.ContainerJSON) (string, error) {
-	utils.Log("RecreateContainer: " + containerID)
-	utils.Log("FROM: " + os.Getenv("HOSTNAME"))
 	if os.Getenv("HOSTNAME") != ""  && os.Getenv("HOSTNAME") == containerID[1:] {
 		err := SelfRecreate()
 		if err != nil {
