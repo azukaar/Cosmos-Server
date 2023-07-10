2447b25422b2dad53dfb10c42b00963c2c5d8451
Yann Stepienik <azukaar@gmail.com>
2023-07-10 13:41:49 +0100

    [release] v0.9.6

--- test-server.js
+++ test-server.js
@@ -1,23 +1,37 @@
-const https = require('https');
 
-const options = {
-  hostname: 'example.com', // Replace with your target URL
-  method: 'OPTIONS',
-};
+const express = require('express')
+const app = express()
+const port = 3000
 
-const req = https.request(options, (res) => {
-  console.log('STATUS:', res.statusCode);
-  console.log('HEADERS:', JSON.stringify(res.headers, null, 2));
-
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
+// console log every request sent
+app.use((req, res, next) => {
+  console.log(`[REQ] - ${req.method} ${req.url}`)
+  next()
 });
 
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
+app.get('/return/:status/:time', async (req, res) => {
+  const statusCode = parseInt(req.params.status);
+  const returnString =`Hello status ${statusCode} after ${req.params.time}ms !`
+
+  console.log(`[RES] - ${statusCode} ${returnString}`)
+  
+  await new Promise(resolve => setTimeout(resolve, req.params.time));
+
+  return res.status(statusCode).send(returnString)
 });
 
-// Close the request
-req.end();
+app.get('/', (req, res) => {
+  console.log("[RES] - Hello World!")
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// app.ws('/ws', function(ws, req) {
+//   ws.on('message', function(msg) {
+//     console.log(msg);
+//     ws.send(msg);
+//   });
+// });
