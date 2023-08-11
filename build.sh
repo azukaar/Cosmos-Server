25707fafff193c0c28139dfb38bbbbf3c2cb6c00
Yann Stepienik <azukaar@gmail.com>
2023-08-11 10:32:23 +0100

    [release] v0.9.19-unstable

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 rm -rf build
+
 env GOARCH=arm64 go build -o build/cosmos-arm64 src/*.go
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 go build -o build/cosmos src/*.go
 if [ $? -ne 0 ]; then
     exit 1
 fi
+
+echo " ---- Build complete, copy assets ----"
+
 cp -r static build/
 cp -r GeoLite2-Country.mmdb build/
 cp -r Logo.png build/
@@ -17,3 +25,5 @@ cat package.json | grep -E '"version"' >> build/meta.json
 echo '  "buildDate": "'`date`'",' >> build/meta.json
 echo '  "built from": "'`hostname`'"' >> build/meta.json
 echo '}' >> build/meta.json
+
+echo " ---- copy complete ----"
