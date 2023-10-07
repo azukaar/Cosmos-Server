c779d475373e88dbd0ffa6352f77ab88cf729667
Yann Stepienik <azukaar@gmail.com>
2023-10-07 14:37:40 +0100

    [release] v0.10.0-unstable30

--- client/src/utils/hooks.js
+++ client/src/utils/hooks.js
@@ -3,22 +3,30 @@ import { useCookies } from 'react-cookie';
 import { logout } from '../api/authentication';
 
 function useClientInfos() {
-  const [cookies] = useCookies(['client-infos']);
+  const [cookies] = useCookies(['client-infos', 'jwttoken']);
+
+  if(cookies['jwttoken'] === undefined) {
+    // probably the demo or new install
+    return {
+      nickname: "",
+      role: "2"
+    }
+  }
   
   let clientInfos = null;
   
   try {
     // Try to parse the cookie into a JavaScript object
     clientInfos = cookies['client-infos'].split(',');
+    
+    return {
+      nickname: clientInfos[0],
+      role: clientInfos[1]
+    };
   } catch (error) {
     console.error('Error parsing client-infos cookie:', error);
     logout();
   }
-  
-  return {
-    nickname: clientInfos[0],
-    role: clientInfos[1]
-  };
 }
 
 export {
