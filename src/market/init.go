6cc644b1b396794227941f48336c0de0e6bf55ec
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2023-10-24 15:55:24 -0400

    Add files via upload

--- src/market/init.go
+++ src/market/init.go
@@ -1,28 +1,88 @@
-package market 
+package market
 
 import (
-	"github.com/azukaar/cosmos-server/src/utils" 
+	"encoding/json"
+	"github.com/azukaar/cosmos-server/src/utils"
+	"os"
 )
 
-func Init() {
-	config := utils.GetMainConfig()
-	currentMarketcache = []marketCacheObject{}
-	sources := config.MarketConfig.Sources
+type MarketConfig struct {
+	DefaultMarket utils.MarketSource
+	Sources       []utils.MarketSource
+}
 
-	// prepend the default market
-	defaultMarket := utils.MarketSource{
-		Url: "https://cosmos-cloud.io/repository",
-		Name: "cosmos-cloud",
-	}
+func Init() {
+	config := ReadMarketConfig()
+	currentMarketcache = []marketCacheObject{}
+	sources := config.Sources
+
+	// Add the default market from the config
+	defaultMarket := config.DefaultMarket
 	sources = append([]utils.MarketSource{defaultMarket}, sources...)
 
 	for _, marketDef := range sources {
 		market := marketCacheObject{
-			Url: marketDef.Url,
+			Url:  marketDef.Url,
 			Name: marketDef.Name,
 		}
 		currentMarketcache = append(currentMarketcache, market)
 
 		utils.Log("MarketInit: Added market " + market.Name)
 	}
+}
+
+func ReadMarketConfig() MarketConfig {
+	configPath := "/config/cosmos.config.json"
+	/*Only the market section is needed, but Sources are avalible to configure as well
+		 *
+		 *
+	    "DefaultMarket": {
+	        "Url": "https://custom-default-market.com",
+	        "Name": "custom-market"
+	    },
+	    "Sources": [
+	        {
+	            "Url": "https://example-repo-1.com",
+	            "Name": "example-repo-1"
+	        },
+	        {
+	            "Url": "https://example-repo-2.com",
+	            "Name": "example-repo-2"
+	        }
+	    ]
+
+
+		 * Add this to the top of the config to change the defaults.
+	*/
+	file, err := os.Open(configPath)
+	if err != nil {
+		// Handle the error (e.g., log it or provide a default config)
+		// You might want to provide a default config here in case the file is missing or invalid.
+		defaultConfig := MarketConfig{
+			DefaultMarket: utils.MarketSource{
+				Url:  "https://cosmos-cloud.io/repository",
+				Name: "cosmos-cloud",
+			},
+			Sources: []utils.MarketSource{},
+		}
+		return defaultConfig
+	}
+	defer file.Close()
+
+	var config MarketConfig
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		// Handle the error (e.g., log it or provide a default config)
+		// You might want to provide a default config here in case the file is invalid.
+		defaultConfig := MarketConfig{
+			DefaultMarket: utils.MarketSource{
+				Url:  "https://cosmos-cloud.io/repository",
+				Name: "cosmos-cloud",
+			},
+			Sources: []utils.MarketSource{},
+		}
+		return defaultConfig
+	}
+
+	return config
 }
