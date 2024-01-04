844d0d481f8b81b58e854218a448d930ffa55973
Kawanaao <kawanaao@zohomail.eu>
2024-01-04 09:30:07 +0200

    Force login to ignore incorrect status by displaying warnings

--- client/src/isLoggedIn.jsx
+++ client/src/isLoggedIn.jsx
@@ -6,17 +6,18 @@ async function isLoggedIn() {
 
     const data = await API.auth.me();
 
-    if (data.status != 'OK') {
-        if (data.status == 'NEW_INSTALL') {
-            return '/cosmos-ui/newInstall';
-        } else if (data.status == 'error' && data.code == "HTTP004") {
-            return '/cosmos-ui/login?redirect=' + redirectToURL;
-        } else if (data.status == 'error' && data.code == "HTTP006") {
-            return '/cosmos-ui/loginmfa?redirect=' + redirectToURL;
-        } else if (data.status == 'error' && data.code == "HTTP007") {
-            return '/cosmos-ui/newmfa?redirect=' + redirectToURL;
-        }
-    } else return data.status
+    if (data.status == 'NEW_INSTALL') {
+        return '/cosmos-ui/newInstall';
+    } else if (data.status == 'error' && data.code == "HTTP004") {
+        return '/cosmos-ui/login?redirect=' + redirectToURL;
+    } else if (data.status == 'error' && data.code == "HTTP006") {
+        return '/cosmos-ui/loginmfa?redirect=' + redirectToURL;
+    } else if (data.status == 'error' && data.code == "HTTP007") {
+        return '/cosmos-ui/newmfa?redirect=' + redirectToURL;
+    } else {
+        console.warn(`Status "${data.status}" does not have a navigation handler, will be ignored!`)
+        return "OK"
+    }
 };
 
 export default isLoggedIn
