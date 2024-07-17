ac258dc22eb7b50c0198e5bd625aae041b8c3e1d
madejackson <martinra1994@gmail.com>
2024-07-17 10:52:05 +0200

    update: - include locale de-CH - better handling of dayjs (works but WIP, see comments in code)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import customParseFormat from 'dayjs/plugin/customParseFormat'; // import this if you need to parse custom formats
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat'; // import this for localized formatting
-import 'dayjs/locale/en-gb';
-import 'dayjs/locale/en';
-import 'dayjs/locale/de';
 
 // import i18n (needs to be bundled ;)) 
 import './utils/locales/i18n';
-import { getLanguage } from './utils/locales/i18n';
+import { dayjsLocale, getLanguage } from './utils/locales/i18n';
 
 // scroll bar
 import 'simplebar/src/simplebar.css';
@@ -32,7 +29,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
 dayjs.extend(customParseFormat); // if needed
 dayjs.extend(localizedFormat); // if needed
-dayjs.locale(getLanguage());
+
+/* // Dynamically loading the dayjs-locale does not work.
+import(`dayjs/locale/${getLanguage().toLowerCase()}.js`).then(() => dayjs.locale(language.toLowerCase()))
+ */
+// Workaround: dynamically load dayjs-locale does only with a const object, which more or less defeats de purpose --> maybe switch from dayjs to luxon or moment.js to not have to import every locale seperately
+dayjsLocale(getLanguage());
 
 // ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
@@ -42,7 +44,7 @@ root.render(
     <StrictMode>
         <ReduxProvider store={store}>
             <BrowserRouter basename="/">    
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={getLanguage()}>
+                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={getLanguage().toLowerCase()}>
                     <App />
                 </LocalizationProvider>
             </BrowserRouter>
