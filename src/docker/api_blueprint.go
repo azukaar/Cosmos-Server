9fc89b704c4f76f4698537d35d62b5d535811812
Ryan Schubert <ryan@stomale.cc>
2024-07-31 17:09:49 -0400

    fix(compose): add runtime to hostConfig, accept clabot

--- src/docker/api_blueprint.go
+++ src/docker/api_blueprint.go
@@ -58,6 +58,7 @@ type ContainerCreateRequestContainer struct {
 
 	Command string `json:"command,omitempty"`
 	Entrypoint string `json:"entrypoint,omitempty"`
+	Runtime string `json:"runtime,omitempty"`
 	WorkingDir string `json:"working_dir,omitempty"`
 	User string `json:"user,omitempty"`
 	UID int `json:"uid,omitempty"`
@@ -694,6 +695,10 @@ func CreateService(serviceRequest DockerServiceCreateRequest, OnLog func(string)
 			CapDrop:     container.CapDrop,
 		}
 
+		if container.Runtime != "" {
+			hostConfig.Runtime = strings.Join(strings.Fields(container.Runtime), " ")
+		}		
+
 		// For Healthcheck
 		if len(container.HealthCheck.Test) > 0 {
 			containerConfig.Healthcheck = &conttype.HealthConfig{
@@ -803,7 +808,6 @@ func CreateService(serviceRequest DockerServiceCreateRequest, OnLog func(string)
 				Was: oldConfig,
 			})
 		}
-		
 		_, err = DockerClient.ContainerCreate(DockerContext, containerConfig, hostConfig, networkingConfig, nil, container.Name)
 
 		if err != nil {
