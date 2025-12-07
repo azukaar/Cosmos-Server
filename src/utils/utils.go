b5828901e7747c1cfeb9c2a643cd550fcff5331a
Mohammad <mohammad.rasim96@gmail.com>
2025-12-07 21:56:12 +0300

    Add the posibility to use propagation wait and disable propagation checks in the letsencrypt module

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -89,6 +89,8 @@ var DefaultConfig = Config{
 		ProxyConfig: ProxyConfig{
 			Routes: []ProxyRouteConfig{},
 		},
+		DisablePropagationChecks:    false,
+		DNSChallengePropagationWait: 30,
 	},
 	DockerConfig: DockerConfig{
 		DefaultDataPath: "/cosmos-storage",
